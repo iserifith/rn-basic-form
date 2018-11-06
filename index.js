31abc568ed99207c0df5e5f0bf56f1beb6d7d4e8
seri <seriputera2@gmail.com>
2018-11-06 19:39:38 +0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,46 @@
+/* eslint-disable */
+
+/**
+ * @author seri <seriputera2@gmail.com>
+ */
+
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+
+class Form extends React.Component {
+  /**
+   * @param {Object} props
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * @returns {ReactComponent}
+   */
+  render() {
+    switch (this.props.type) {
+      case 'TextInput':
+        return (
+          <View style={this.props.containerStyle}>
+            <Text style={this.props.titleStyle}>{this.props.title}</Text>
+            <TextInput
+              style={this.props.textInputStyle}
+              placeholder={this.props.placeholder}
+              onChangeText={value => this.props.onChangeText(value)}
+            >
+              {this.props.value}
+            </TextInput>
+            <Text style={this.props.errorStyle}>
+              {this.props.error || null}
+            </Text>
+          </View>
+        );
+
+      default:
+        break;
+    }
+  }
+}
+
+export default Form;
